78aa27c007823ad1b322230ea3becbd3bef2ac74
Kačer Šimon <kacers21@spstrutnov.cz>
2022-09-26 19:29:34 +0200

    Mapa úspěšně vykreslena do pygame.

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 import pygame, sys
 import math
 
-# mapa ######################################################################################
+#  ######################################################################################
 
 VELIKOST_RASTRU = 100
 
@@ -18,43 +18,70 @@ mapa = [
         [0,1,2,3,4,5,6,7,8,9,10], #7
         [0,1,2,3,4,5,6,7,8,9,10], #8
         [0,1,2,3,4,5,6,7,8,9,10], #9
-        ]
+    ]
+
+
 # levý horní roh
-A = mapa[0],[0]
-B = mapa[0],[1]
-C = mapa[0],[2]
-D = mapa[1],[0]
-E = mapa[1],[1]
-F = mapa[2],[0]
+A = [0,0]
+B = [0,1]
+C = [0,2]
+D = [1,0]
+E = [1,1]
+F = [2,0]
 
 # pravý horní roh
-G = mapa[0],[8]
-H = mapa[0],[9]
-CH = mapa[0],[10]
-I = mapa[1],[9]
-J = mapa[1],[10]
-K = mapa[2],[10]
+G = [0,8]
+H = [0,9]
+CH = [0,10]
+I = [1,9]
+J = [1,10]
+K = [2,10]
 
 # levý dolní roh
-L = mapa[7],[0]
-M = mapa[8],[0]
-O = mapa[8],[1]
-P = mapa[9],[0]
-Q = mapa[9],[1]
-R = mapa[9],[2]
+L = [7,0]
+M = [8,0]
+O = [8,1]
+P = [9,0]
+Q = [9,1]
+R = [9,2]
 
 # pravý horní roh
-S = mapa[9],[8]
-T = mapa[9],[9]
-U = mapa[9],[10]
-V = mapa[8],[9]
-W = mapa[8],[10]
-Z = mapa[7],[10]
+S = [9,8]
+T = [9,9]
+U = [9,10]
+V = [8,9]
+W = [8,10]
+Z = [7,10]
+
+# střed
+aA = [4,1]
+bB = [5,1]
+cC = [3,2]
+dD = [4,2]
+eE = [5,2]
+fF = [6,2]
+gG = [3,4]
+hH = [3,5]
+chCH = [3,6]
+iI = [2,5]
+jJ = [6,4]
+kK = [6,5]
+lL = [6,6]
+mM = [7,5]
+nN = [3,8]
+oO = [4,8]
+pP = [5,8]
+qQ = [6,8]
+rR = [4,9]
+sS = [5,9]
+    
+ctverecky = [A,B,C,D,E,F,G,H,CH,I,J,K,L,M,O,P,Q,R,S,T,U,V,W,Z,aA,bB,cC,dD,eE,fF,gG,hH,chCH,iI,jJ,kK,lL,mM,nN,oO,pP,qQ,rR,sS]
 
 # proměnné ##################################################################################
 
 ROZLISENI_OKNA = ROZLISENI_X, ROZLISENI_Y = 1100,1000
 RGB = R, G, B, = 255, 255, 255
+cerna = 0, 0, 0
 
 
 #TĚLESA/TANKY
@@ -109,7 +136,10 @@ while True:
 # vykreslovani ##############################################################################
     
     okno.fill(RGB)
-    #pygame.draw.rect(okno,)
+    
+    for ctverecek in ctverecky:
+        pygame.draw.rect(okno, cerna,((ctverecek[1]*VELIKOST_RASTRU,ctverecek[0]*VELIKOST_RASTRU), (100,100)))
+    
     pygame.draw.circle(okno, (205,8,0), (x1,y1), r1)
     pygame.display.update()
         
