100447e949abde5faa7ff3da235871cd06ee6381
Kačer Šimon <kacers21@spstrutnov.cz>
2022-11-06 10:29:32 +0100

    Čudlíky v in_game_menu mají svoje vlastní nadpisy.

--- src/main.py
+++ src/main.py
@@ -22,6 +22,7 @@ mapa3_pozadi = pygame.image.load("..\doc\mapa-3.png")
 pygame.font.init()
 
 typ_pisma = pygame.font.Font('freesansbold.ttf', 25)
+typ_pisma_in_game_menu = pygame.font.SysFont('Berlin Sans FB', 26)
 
 # cudliky
 #hlavní menu
@@ -45,7 +46,7 @@ cl_close2 = ((25, 725), (150, 50), (0,0,0), "text")
 #ingame menu
 np4 = ((365, 175), (400,75), (0,0,0), "text" )
 cl_exit2 = ((492, 420), (150, 50), (0,0,0), "text")
-cl_close3 = ((340, 580), (150, 50), (0,0,0), "text")
+cl_close3 = ((340, 580), (210, 50), (0,0,0), "text")
 cl_pin = ((492, 325), (150, 50), (0,0,0), "text")
 
 # obrazovky menu
@@ -366,7 +367,7 @@ while True:
     pygame.draw.rect(okno, (255, 8, 0), hrac2.rect)
     pygame.draw.rect(okno, (0, 200, 0), hrac1.rect)
     
-# cudliky v pause menu
+# cudliky v pause menu 
 
     if in_game_menu == True:
         aktivni_obrazovka = pause_menu
@@ -387,7 +388,29 @@ while True:
             MENU = True
         
         if cl_pin[0][0] < pygame.mouse.get_pos()[0] < (cl_pin[0][0] + cl_pin[1][0]) and cl_pin[0][1] < pygame.mouse.get_pos()[1] < (cl_pin[0][1] + cl_pin[1][1]) and pygame.mouse.get_pressed()[0]:    
-            print("ahoj")    
+            pass
+        
+        hl_nadpis4 = typ_pisma_in_game_menu.render('Panzerschlachtfeld im Labyrinth:', True, bila, cerna)
+        hl_nadpis4Rect = hl_nadpis4.get_rect()
+        hl_nadpis4Rect.center = (565, 210)
+        
+        nadpis_exit1 = typ_pisma_in_game_menu.render('Exit', True, bila, cerna)
+        nadpis_exit1Rect = nadpis_exit1.get_rect()
+        nadpis_exit1Rect.center = (565, 442)
+        
+        nadpis_close3 = typ_pisma_in_game_menu.render('Zpátky do menu', True, bila, cerna)
+        nadpis_close3Rect = nadpis_close3.get_rect()
+        nadpis_close3Rect.center = (445, 603)
+        
+        nadpis_pin = typ_pisma_in_game_menu.render('Pauza', True, bila, cerna)
+        nadpis_pinRect =  nadpis_pin.get_rect()
+        nadpis_pinRect.center = (565, 348)
+        
+        okno.blit(hl_nadpis4, hl_nadpis4Rect)
+        okno.blit(nadpis_exit1, nadpis_exit1Rect)
+        okno.blit(nadpis_close3, nadpis_close3Rect)
+        okno.blit(nadpis_pin, nadpis_pinRect)
+        
 
     clockobject = pygame.time.Clock()
     clockobject.tick(rychlost)
